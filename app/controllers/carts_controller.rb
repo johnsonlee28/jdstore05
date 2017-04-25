7f6ea55fec36b70c4bd9b27c03d3acd82492df11
Johnsonlee <johnsonlee28@gmail.com>
2017-04-25 10:43:12 +0800

    add empty cart button

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,8 @@
 class CartsController < ApplicationController
-  
+
+  def empty
+    current_cart.clean!
+    redirect_to carts_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,9 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :carts
+  resources :carts do
+    collection do
+      delete :empty
+    end
+  end
 end
