a48783660506206bb7722c910ba126cc9cd5fa82
cuishirui <344494024@qq.com>
2017-04-28 23:44:48 +0800

    implement search function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :validate_search_key, only: [:search]
   def index
     @products = Product.all
   end
@@ -11,11 +12,28 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     if !current_cart.products.include?(@product)
       current_cart.add_product_to_cart(@product)
-      redirect_to :back, notice:"Added to cart"
+      redirect_to :back, notice:"#{@product.title} 已加入购物车"
     else
       redirect_to :back, alert:"购物车已存在#{@product.title}商品"
     end
+  end
 
+  def search
+    if @query_string.present?
+      search_result = Product.ransack(@search_criteria).result(distinct: true)
+      @products = search_result.paginate(page: params[:page], per_page: 5)
+    end
+  end
+
+  protected
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+    @search_criteria = search_criteria(@query_string)
+  end
+
+  def search_criteria(query_string)
+    { :title_cont => query_string }
   end
 
 end
