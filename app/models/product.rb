87e72938e53e7a59132aad3fb1ae9e6aba2853ca
Johnsonlee <johnsonlee28@gmail.com>
2017-05-05 11:14:29 +0800

    add like function with ajax

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,13 @@ class Product < ApplicationRecord
   belongs_to :category
 
   has_many :photos
-  accepts_nested_attributes_for :photos 
+
+  has_many :likes, :dependent => :destroy
+  has_many :liked_users, :through => :likes, :source => :user
+
+  def find_like(user)
+    self.likes.where( :user_id => user.id).first
+  end
+
+  accepts_nested_attributes_for :photos
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
 
   has_many :orders
 
+  has_many :likes, :dependent => :destroy
+  has_many :liked_products, :through => :likes, :source => :product
+
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#"}, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
