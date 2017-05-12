9b2059e31439873e5d01077ef294968ece990091
cuishirui <344494024@qq.com>
2017-05-12 14:09:34 +0800

    upload introduce photo in seed

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -78,7 +78,7 @@ class Admin::ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :quantity, :price, :image, :category_id)
+    params.require(:product).permit(:title, :description, :quantity, :price, :image, :category_id, :detail)
   end
 
 end
