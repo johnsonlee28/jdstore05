bc7b043ad8372f71448d52595d91b357283ce955
Johnsonlee <johnsonlee28@gmail.com>
2017-05-23 17:00:07 +0800

    modify navbar cart number

--- db/schema.rb
+++ db/schema.rb
@@ -123,12 +123,6 @@ ActiveRecord::Schema.define(version: 20170522053556) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "subscribers", force: :cascade do |t|
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
